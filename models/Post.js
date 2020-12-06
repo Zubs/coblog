72511231cdd8767c6a33f1da6acd59b82f64abd5
Efusanya Ayomide <efusanyaae@gmail.com>
2020-12-06 15:25:26 +0100

    restructured models

--- models/Post.js
+++ models/Post.js
@@ -1,18 +1,28 @@
 // Import things I need to import
 const mongoose = require('mongoose');
+const User = require('./User');
 const Schema = mongoose.Schema;
 
 // Create the Schema
 const PostSchema = new Schema({
 	title: {
 		type: String,
-		required: true
+		required: [true, 'You have to give your write-up a title.']
 	},
 	body: {
 		type: String,
-		required: true
+		required: [true, 'Your write-up must have some content.']
 	},
-	
+	uid:{
+		type: mongoose.SchemaTypes.ObjectId,
+		ref:"User",
+		required:true,
+	},
+	tag:{
+		type: String,
+		required:false,
+		unique:true,
+	}
 	// More code to be added soon
 }, {
 	timestamps: true
