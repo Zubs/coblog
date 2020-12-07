974b3f96d6b12c449b3a84217126225d90137282
Zubs <zubairidrisaweda@gmail.com>
2020-12-07 19:57:43 +0100

    Done with middlewares

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -7,10 +7,10 @@ module.exports = (req, res, next) => {
 
     // Check if user exists at all incase the user tries something funny 🤨 
     
-  User.findById(req.session.userId, (error, user) => {
+  	User.findById(req.session.userId, (error, user) => {
       
-      // If he or she or they were actually crooks send their asses back to login
-    if (error || !user) return res.redirect("/login");
-    next();
-  });
+    	// If he or she or they were actually crooks send their asses back to login
+    	if (error || !user) return res.redirect("/login");
+    	next();
+  	});
 };
